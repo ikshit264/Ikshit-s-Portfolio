8c2f249952f9d82837fd1164411294f20ac68d84
ikshit264 <ikshit.talera@gmail.com>
2025-03-21 17:04:46 +0530

    seo improved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { StateProvider } from "@/hooks/StateContext";
@@ -11,14 +11,19 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
-      {/* <head>
-        <link rel="icon" href="/public/1.svg" type="image/svg+xml" />
-      </head> */}
+      <head>
+        <title>My Portfolio | Windows XP Experience</title>
+        <meta name="description" content="Welcome to My Portfolio, where you get a nostalgic experience of Windows XP." />
+        <meta name="keywords" content="Next.js, React, Web App, Modern UI, Ikshit, Portfolio, Windows XP" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/icon.png" type="image/svg+xml" />
+      </head>
       <body className={`${inter.className}`}>
-        <StateProvider>{children}</StateProvider>
+        <StateProvider>
+          {children}
+        </StateProvider>
         <Analytics />
       </body>
     </html>
